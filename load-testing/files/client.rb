4a06131d264ece58e2b30342086d7f0832d7bc70
Ole Diederich <oled@lshift.de>
2016-09-29 15:17:30 +0200

    Early load testing automation version

---
+++ load-testing/files/client.rb
@@ -0,0 +1,18 @@
+require "bunny"
+require "readline"
+
+conn = Bunny.new :host => ARGV[0], :user => ARGV[1], :pass => ARGV[2]
+conn.start
+ch = conn.create_channel
+q  = ch.queue("bunny")
+x  = ch.default_exchange
+
+q.subscribe do |delivery_info, metadata, payload|
+  puts "Received #{payload}"
+end
+
+for i in 0..ARGV[3]
+  x.publish("Hello #{i}!", :routing_key => q.name)
+end
+
+conn.close
